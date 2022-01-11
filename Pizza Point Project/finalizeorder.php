aa0d47c4049f431a47d85d4656b2024cd694bb9d
jankoskiantonio <94110698+jankoskiantonio@users.noreply.github.com>
2022-01-11 15:27:54 +0100

    Add files via upload

--- Pizza Point Project/finalizeorder.php
+++ Pizza Point Project/finalizeorder.php
@@ -22,6 +22,13 @@ $orderText = substr($orderText, 0, -2);
 
 $query="insert into orderlist values(NULL, '$orderText', '$date', '$addressText',  $orderPrice, $phone)";
 if(mysqli_query($conn,$query)){
+    unset($_SESSION['cart']);
+    echo '<script>alert("Checkout successful. Your order will be delivered soon.");</script>';
+    $query="truncate table orders";
+    mysqli_query($conn, $query);
     header("refresh:0;url=index.php");
+}
+else{
+    echo '<script>alert("Something went wrong. Make sure your credentials are inserted correctly or try again later.");</script>';
 }
 ?>
